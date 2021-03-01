0b4abbadafb03db15931f698c8b9e1795dc268c0
ArtNight-0 <arohendi90@gmail.com>
2021-03-01 13:36:34 +0700

    "commit3"

---
+++ app/src/main/java/com/example/project_nusantarafit/BottomShettDialog.java
@@ -0,0 +1,21 @@
+package com.example.project_nusantarafit;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+public class BottomShettDialog extends BottomSheetDialogFragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v  = inflater.inflate(R.layout.payment_popup, container,false);
+
+        return  v;
+    }
+}
