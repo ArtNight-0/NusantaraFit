03c7bddaed7de1328f8ce9efaa48b4a6037c50ec
ArtNight-0 <arohendi90@gmail.com>
2021-03-24 01:03:55 +0700

    "Revisi camera_fragment.xml, Add item_search_display_kategori.xml, add item_search_display_populer.xml, activity_item_room_chat.xml "

--- app/src/main/java/com/example/project_nusantarafit/CameraFragment.java
+++ app/src/main/java/com/example/project_nusantarafit/CameraFragment.java
@@ -1,19 +1,59 @@
 package com.example.project_nusantarafit;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Camera;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 public class CameraFragment extends Fragment {
-
+    ImageView imageView;
+    Button btn;
 
     public View onCreateView(@Nullable LayoutInflater inflater,
                              @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.camera_fragment, null);
 
+//        imageView = imageView.findViewById(R.id.image_view);
+//        btn = btn.findViewById(R.id.btnOpen);
+//
+//        if(ContextCompat.checkSelfPermission(CameraFragment.this,
+//                Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+//            ActivityCompat.requestPermissions(CameraFragment.this, new String[]{
+//                            Manifest.permission.CAMERA
+//                    },
+//                    100);
+//        }
+//        btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//                startActivityForResult(intent,100);
+//            }
+//        });
+
     }
+
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        if (requestCode == 100) {
+//            Bitmap capture = (Bitmap) data.getExtras().get("data");
+//            //set
+//            imageView.setImageBitmap(capture);
+//        }
+//    }
+
 }
