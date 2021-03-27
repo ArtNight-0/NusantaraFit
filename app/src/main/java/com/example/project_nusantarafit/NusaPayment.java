32c6d5834187ec5ac3f43f47f65effb3ce63a523
VirgiawanD <virgiawandejan@gmail.com>
2021-03-27 23:17:48 +0700

    "Virgi Menambahkan point.xml"

---
+++ app/src/main/java/com/example/project_nusantarafit/NusaPayment.java
@@ -0,0 +1,31 @@
+package com.example.project_nusantarafit;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class NusaPayment extends AppCompatActivity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.nusantara_pay);
+
+        ArrayList<Pay> arrayPay = new ArrayList<>();
+        PayAdapter adapter = new PayAdapter(this, arrayPay);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(adapter);
+
+        Pay pay = new Pay(R.drawable.logo, "Fill in the balance", "11.12.21", "+ 7.750.000", "Success");
+        adapter.add(pay);
+
+        pay = new Pay(R.drawable.logo, "Payment", "11.12.21", "+ 10.000.000", "Succes");
+        adapter.add(pay);
+
+        pay = new Pay(R.drawable.logo, "Transfer", "11.12.21", "+ 100.000.000", "Failed");
+        adapter.add(pay);
+    }
+}
