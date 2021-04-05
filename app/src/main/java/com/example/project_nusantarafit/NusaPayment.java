ca7be436d93789486ca8874e0e5fc5e71d764234
VirgiawanD <virgiawandejan@gmail.com>
2021-04-05 12:48:17 +0700

    firebase rev

--- app/src/main/java/com/example/project_nusantarafit/NusaPayment.java
+++ app/src/main/java/com/example/project_nusantarafit/NusaPayment.java
@@ -1,31 +1,38 @@
 package com.example.project_nusantarafit;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ListView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import java.util.ArrayList;
 
-public class NusaPayment extends AppCompatActivity{
+public class NusaPayment extends Fragment {
 
+    @Nullable
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.nusantara_pay);
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState){
+        return inflater.inflate(R.layout.nusantara_pay, null);
 
-        ArrayList<Pay> arrayPay = new ArrayList<>();
-        PayAdapter adapter = new PayAdapter(this, arrayPay);
-        ListView listView = (ListView) findViewById(R.id.list);
-        listView.setAdapter(adapter);
-
-        Pay pay = new Pay(R.drawable.logo, "Fill in the balance", "11.12.21", "+ 7.750.000", "Success");
-        adapter.add(pay);
-
-        pay = new Pay(R.drawable.logo, "Payment", "11.12.21", "+ 10.000.000", "Succes");
-        adapter.add(pay);
-
-        pay = new Pay(R.drawable.logo, "Transfer", "11.12.21", "+ 100.000.000", "Failed");
-        adapter.add(pay);
+//        ArrayList<Pay> arrayPay = new ArrayList<>();
+//        PayAdapter adapter = new PayAdapter(this, arrayPay);
+//        ListView listView = (ListView) findViewById(R.id.list);
+//        listView.setAdapter(adapter);
+//
+//        Pay pay = new Pay(R.drawable.logo, "Fill in the balance", "11.12.21", "+ 7.750.000", "Success");
+//        adapter.add(pay);
+//
+//        pay = new Pay(R.drawable.logo, "Payment", "11.12.21", "+ 10.000.000", "Succes");
+//        adapter.add(pay);
+//
+//        pay = new Pay(R.drawable.logo, "Transfer", "11.12.21", "+ 100.000.000", "Failed");
+//        adapter.add(pay);
     }
 }
