4ff203adabb4d82b7c65b72dc9e5ff820be98d24
ArtNight-0 <arohendi90@gmail.com>
2021-04-14 04:19:23 +0700

    "POJO dan intent quiz"

--- app/src/main/java/com/example/project_nusantarafit/HomeFragment.java
+++ app/src/main/java/com/example/project_nusantarafit/HomeFragment.java
@@ -6,9 +6,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.SearchView;
 
 import androidx.annotation.Nullable;
-import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 
 public class HomeFragment extends Fragment {
--- app/src/main/java/com/example/project_nusantarafit/QuizFragment.java
+++ app/src/main/java/com/example/project_nusantarafit/QuizFragment.java
@@ -12,15 +12,18 @@ import androidx.fragment.app.Fragment;
 
 public class QuizFragment extends Fragment {
     @Nullable
+    Button btn_quiz1;
+    Button btn_quiz2;
+    Button btn_quiz3;
+    Button btn_quiz4;
     @Override
     public View onCreateView(LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState){
         View rootView = inflater.inflate(R.layout.nusantara_quiz_fragment,
                 container, false);
-
-        Button button = (Button) rootView.findViewById(R.id.btn_quiz1);
-        button.setOnClickListener(new View.OnClickListener() {
+        Button btn_quiz1 = (Button) rootView.findViewById(R.id.btn_quiz1);
+        btn_quiz1.setOnClickListener(new View.OnClickListener() {
         @Override
             public void onClick(View v){
 
@@ -28,6 +31,38 @@ public class QuizFragment extends Fragment {
             startActivity(intent);
         }
         });
+
+        Button btn_quiz2 = (Button) rootView.findViewById(R.id.btn_quiz2);
+        btn_quiz2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+
+                Intent intent = new Intent(getActivity() ,quiz1.class);
+                startActivity(intent);
+            }
+        });
+
+        Button btn_quiz3 = (Button) rootView.findViewById(R.id.btn_quiz1);
+        btn_quiz3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+
+                Intent intent = new Intent(getActivity() ,quiz1.class);
+                startActivity(intent);
+            }
+        });
+
+        Button btn_quiz4 = (Button) rootView.findViewById(R.id.btn_quiz1);
+        btn_quiz4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+
+                Intent intent = new Intent(getActivity() ,quiz1.class);
+                startActivity(intent);
+            }
+        });
+
+
         return rootView;
     }
 }
