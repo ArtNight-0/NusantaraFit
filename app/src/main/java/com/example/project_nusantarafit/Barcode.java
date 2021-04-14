10918d36d3f09c7cce77e0e1a68f508f10d0a02e
angganabila18.siswa@smkn4bdg.sch.id <angganabila18.siswa@smkn4bdg.sch.id>
2021-04-14 20:44:44 +0700

    Menambahkan fungsi camera Barcode

--- app/src/main/java/com/example/project_nusantarafit/Barcode.java
+++ app/src/main/java/com/example/project_nusantarafit/Barcode.java
@@ -2,12 +2,44 @@ package com.example.project_nusantarafit;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import android.util.Log;
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+import com.google.zxing.Result;
 
-public class Barcode extends AppCompatActivity {
+public class Barcode extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+    private ZXingScannerView mScannerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.nusantara_barcode_scan);
+
+        mScannerView = new ZXingScannerView(this);
+        setContentView(mScannerView);
+    }
+    @Override
+    public void onResume(){
+        super.onResume();
+        mScannerView.setResultHandler(this);
+        mScannerView.startCamera();
+    }
+    @Override
+    public void onPause(){
+        super.onPause();
+        mScannerView.stopCamera();
+    }
+    @Override
+    public void handleResult(Result rawResult){
+        Log.v("TAG", rawResult.getText());
+        Log.v("TAG", rawResult.getBarcodeFormat().toString());
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Hasil Scan");
+        builder.setMessage(rawResult.getText());
+        AlertDialog alert1 = builder.create();
+        alert1.show();
+
+        mScannerView.resumeCameraPreview(this);
     }
 }
